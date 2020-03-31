0bab7d3b73efb07a2905aca419f695b3e9712b0f
Anil Poriya <anil@altiscale.com>
2020-03-31 15:48:15 -0700

    The scripts for copying the contents of bower packages in the respective bower_components folders as required to avoid front-end errors duing grunt build

---
+++ sap-xmake-bower/scripts/copy-tez-ui2-bower.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cp -a ${TEZ_UI2_BOWERRC_DIR}/FileSaver.js-1.20150507.2/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/file-saver.js/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/Font-Awesome-4.5.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/font-awesome/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/Numeral-js-1.5.3/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/numeral/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/bootstrap-3.3.6/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/bootstrap/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/codemirror-5.11.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/codemirror/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/ember-2.2.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/ember/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/ember-cli-shims-0.0.6/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/ember-cli-shims/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/ember-cli-test-loader-0.2.1/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/ember-cli-test-loader/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/ember-data-2.1.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/ember-data/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/ember-load-initializers-0.1.7/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/ember-load-initializers/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/ember-qunit-builds-0.4.16/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/ember-qunit/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/ember-qunit-notifications-0.1.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/ember-qunit-notifications/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/jquery-dist-2.1.4/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/jquery/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/jquery-mousewheel-3.1.13/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/jquery-mousewheel/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/loader.js-3.3.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/loader.js/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/moment-2.12.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/moment/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/moment-timezone-0.5.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/moment-timezone/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/more-js-0.8.2/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/more-js/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/qunit-1.19.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/qunit/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/snippet-ss-1.11.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/snippet-ss/
+cp -a ${TEZ_UI2_BOWERRC_DIR}/zip.js-1.0.0/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/zip-js/
+
+# Since jquery-ui-1.11.4 was installed as part of tez-ui, it is fetched from the cache,
+# it is not present in the tez-ui2 directory.
+# Hence we copy it from the tez-ui directory which has the same jquery-ui version
+cp -a ${TEZ_UI_BOWERRC_DIR}/jqueryui-1.11.4/. ${TEZ_UI2_BOWERRC_DIR}/bower_components/jquery-ui/
