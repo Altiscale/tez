192afc6cf33035462cb19b978a32a743eb608604
Anil Poriya <anil@altiscale.com>
2020-04-02 19:03:31 -0700

    Removing existing folders insider bower_components before moving package folders

--- sap-xmake-bower/scripts/copy-tez-ui-bower.sh
+++ sap-xmake-bower/scripts/copy-tez-ui-bower.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-#cp -a ${TEZ_UI_BOWERRC_DIR}/Font-Awesome-4.2.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/font-awesome/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/bootstrap-3.3.1/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/bootstrap/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/bootstrap-for-ember-0.7.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-addons.bs_for_ember/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/codemirror-5.2.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/codemirror/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/d3-bower-3.4.11/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/d3/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/ember-1.7.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/ember-data-1.0.0-beta.11/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-data/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/ember-i18n-2.9.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-i18n/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/ember-json-mapper-1.0.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-json-mapper/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/ember-table-0.2.4/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-table/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/jqueryui-1.11.4/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/jquery-ui/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/moment-2.10.6/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/moment/
-#cp -a ${TEZ_UI_BOWERRC_DIR}/moment-timezone-0.4.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/moment-timezone/
+# bower install fails to install bower packages and creates empty package folders inside app/bower_components.
+# This results in an undesired directory structure.
+
+rm -rf ${TEZ_UI_BOWERRC_DIR}/app/bower_components/*
 
 mv ${TEZ_UI_BOWERRC_DIR}/Font-Awesome-4.2.0 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/font-awesome
 mv ${TEZ_UI_BOWERRC_DIR}/bootstrap-3.3.1 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/bootstrap
@@ -24,6 +15,6 @@ mv ${TEZ_UI_BOWERRC_DIR}/ember-data-1.0.0-beta.11 ${TEZ_UI_BOWERRC_DIR}/app/bowe
 mv ${TEZ_UI_BOWERRC_DIR}/ember-i18n-2.9.0 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-i18n
 mv ${TEZ_UI_BOWERRC_DIR}/ember-json-mapper-1.0.0 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-json-mapper
 mv ${TEZ_UI_BOWERRC_DIR}/ember-table-0.2.4 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-table
-cp -a ${TEZ_UI_BOWERRC_DIR}/jqueryui-1.11.4/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/jquery-ui/
 mv ${TEZ_UI_BOWERRC_DIR}/moment-2.10.6 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/moment
 mv ${TEZ_UI_BOWERRC_DIR}/moment-timezone-0.4.0 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/moment-timezone
+mv ${TEZ_UI_BOWERRC_DIR}/jqueryui-1.11.4 ${TEZ_UI_BOWERRC_DIR}/app/bower_components/jquery-ui
