301dfd5923ca64a1ef773837fd88b28817f6cb88
Anil Poriya <anil@altiscale.com>
2020-04-13 00:43:01 -0700

    The scripts for copying the contents of bower packages in the respective bower_components folders as required to avoid front-end errors duing grunt build

---
+++ sap-xmake-bower/scripts/copy-tez-ui-bower.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp -a ${TEZ_UI_BOWERRC_DIR}/Font-Awesome-4.2.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/font-awesome/
+cp -a ${TEZ_UI_BOWERRC_DIR}/bootstrap-3.3.1/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/bootstrap/
+cp -a ${TEZ_UI_BOWERRC_DIR}/bootstrap-for-ember-0.7.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-addons.bs_for_ember/
+cp -a ${TEZ_UI_BOWERRC_DIR}/d3-bower-3.4.11/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/d3/
+cp -a ${TEZ_UI_BOWERRC_DIR}/ember-1.7.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember/
+cp -a ${TEZ_UI_BOWERRC_DIR}/ember-data-1.0.0-beta.11/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-data/
+cp -a ${TEZ_UI_BOWERRC_DIR}/ember-i18n-2.9.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-i18n/
+cp -a ${TEZ_UI_BOWERRC_DIR}/ember-json-mapper-1.0.0/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-json-mapper/
+cp -a ${TEZ_UI_BOWERRC_DIR}/ember-table-0.2.4/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/ember-table/
+cp -a ${TEZ_UI_BOWERRC_DIR}/jqueryui-1.11.4/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/jquery-ui/
+cp -a ${TEZ_UI_BOWERRC_DIR}/moment-2.8.4/. ${TEZ_UI_BOWERRC_DIR}/app/bower_components/moment/
